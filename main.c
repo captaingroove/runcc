9f096288b3c69cf0dbe2e1b1675905af2def34e8
Jörg Bakker <captaingroove@openmultimedia.org>
2022-11-20 15:20:07 +0100

    Split main into some functions

--- main.c
+++ main.c
@@ -9,38 +9,67 @@
 #define BUILD_PATH_MAX    64
 #define CMD_MAX         1024
 
-static char script_path[FILE_NAME_MAX + BUILD_PATH_MAX];
+static char  ccode_path[FILE_NAME_MAX + BUILD_PATH_MAX];
 static char    exe_path[FILE_NAME_MAX + BUILD_PATH_MAX];
 static char    comp_cmd[CMD_MAX];
 static char     run_cmd[CMD_MAX];
+size_t script_size = 0;
+char *script_ptr = NULL, *ccode_start = NULL;
+
+
+char *
+find_ccode_start(char *script_ptr, size_t script_size)
+{
+	char *ccode_start = script_ptr;
+	// Skip shebang line if exists
+	if (script_ptr[0] == '#' && script_ptr[1] == '!') {
+		ccode_start = strchr(script_ptr, '\n');
+		ccode_start++;
+	}
+	return ccode_start;
+}
+
+
+void
+get_paths(char *script_path, char *ccode_path, char *exe_path)
+{
+	char *script_name = qfile_get_name(script_path);
+	char *build_dir = "/tmp";
+	sprintf(ccode_path, "%s/%s", build_dir, script_name);
+	sprintf(exe_path, "%s/%s", build_dir, script_name);
+	exe_path[strlen(exe_path) - 2] = '\0';
+}
+
+
+void
+write_ccode_compile_and_run(
+	char *ccode_start,
+	size_t ccode_size,
+	char *ccode_path,
+	char *exe_path)
+{
+	qfile_save(ccode_path, ccode_start, ccode_size, false);
+	sprintf(comp_cmd, "gcc %s -o %s", ccode_path, exe_path);
+	system(comp_cmd);
+	sprintf(run_cmd, "%s", exe_path);
+	system(run_cmd);
+}
 
 
 int
 main(int argc, char *argv[])
 {
 	if (argc == 1) {
-		printf("usage: %s script.c\n", argv[0]);
+		printf("usage: %s <script.c>\n", argv[0]);
 		return EXIT_SUCCESS;
 	}
-	size_t script_size;
-	char *script_ptr = qfile_load(argv[1], &script_size);
-	// Skip shebang line if exists
-	char *script_start = script_ptr;
-	if (script_ptr[0] == '#' && script_ptr[1] == '!') {
-		script_start = strchr(script_ptr, '\n');
-		script_start++;
-	}
-	// Script source and exe file names
-	char *script_name = qfile_get_name(argv[1]);
-	char *build_dir = "/tmp";
-	sprintf(script_path, "%s/%s", build_dir, script_name);
-	sprintf(exe_path, "%s/%s", build_dir, script_name);
-	exe_path[strlen(exe_path) - 2] = '\0';
-	// Save script to file after processing, compile, and run
-	qfile_save(script_path, script_start, script_size - (script_start - script_ptr), false);
-	sprintf(comp_cmd, "gcc %s -o %s", script_path, exe_path);
-	system(comp_cmd);
-	sprintf(run_cmd, "%s", exe_path);
-	system(run_cmd);
+	script_ptr = qfile_load(argv[1], &script_size);
+	ccode_start = find_ccode_start(script_ptr, script_size);
+	get_paths(argv[1], ccode_path, exe_path);
+	write_ccode_compile_and_run(
+		ccode_start,
+		script_size - (ccode_start - script_ptr),
+		ccode_path,
+		exe_path);
 	return EXIT_SUCCESS;
 }
