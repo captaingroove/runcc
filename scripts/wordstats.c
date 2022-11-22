a7f66fa33d29c7c394e63482defe3c2b3b4ac873
Jörg Bakker <captaingroove@openmultimedia.org>
2022-11-22 16:42:08 +0100

    Finished wordstats script

--- scripts/wordstats.c
+++ scripts/wordstats.c
@@ -1,6 +1,8 @@
 #!/usr/bin/env runcc
 #libs qlibc pthread
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <qlibc/qlibc.h>
 #include <qlibc/utilities/qfile.h>
 
@@ -9,15 +11,19 @@ main(int argc, char *argv[])
 {
 	if (argc == 1) { printf("usage: %s <file>\n", argv[0]); return 0; }
 	size_t fsize;
-	char *ifile = qfile_load(argv[1], &fsize);
-	qlist_t *words = qstrtokenizer(ifile, " \t\r\n");
+	char *file = qfile_load(argv[1], &fsize);
+	qlist_t *words = qstrtokenizer(file, " \t\r\n");
 	qhashtbl_t *wordmap = qhashtbl(0, 0);
 	char *w; int wc = 0;
 	while ((w = qlist_popfirst(words, NULL))) {
 		if (strlen(w)) {
-			printf("%s\n", w);
+			qhashtbl_putint(wordmap, w, qhashtbl_getint(wordmap, w) + 1);
 			wc++;
 		}
 	}
-	printf("word count: %d\n", wc);
+	printf("Total word count: %d\nDifferent words: %ld\nWordmap:\n", wc, qhashtbl_size(wordmap));
+	qhashtbl_obj_t o = {0};
+	while (qhashtbl_getnext(wordmap, &o, false)) {
+		printf("  '%s': %s\n", o.name, (char*)o.data);
+	}
 }
